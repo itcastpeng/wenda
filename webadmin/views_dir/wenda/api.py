a913d1b7fb3347a009e86d60fc3885bd09110e79
zhaoxinpeng <15531506906@sina.cn>
2018-05-18 16:15:19 +0800

    跨表查询修改查询条件

--- webadmin/views_dir/wenda/api.py
+++ webadmin/views_dir/wenda/api.py
@@ -546,13 +546,13 @@ def check_wenda_link(request):
             status = request.POST.get('status')
             tid = request.POST.get('tid')
 
-            obj = models.WendaLink.objects.get(id=tid)
+            obj = models.WendaLink.objects.select_related('task').get(id=tid)
             obj.status = status
             obj.update_date = datetime.datetime.now()
             obj.save()
 
             if obj.status == '2':
-                wenda_objs = obj.task.wendarobottask_set.select_related('wenda_url','title','content').filter(wenda_url=obj.url)
+                wenda_objs = obj.task.wendarobottask_set.filter(wenda_url=obj.url)
                 print('wenda_objs -  -> ', wenda_objs)
                 wenda_obj = wenda_objs[0]
                 models.TongjiKeywords.objects.create(
