3f05d45d233c5714396ad497187a1a8f8c95a4ba
zhaoxinpeng <15531506906@sina.cn>
2018-06-01 19:15:49 +0800

    问答，顾问对接表，其余功能 增加一张显示客户详情表

--- webadmin/forms/guwen_duijie_biao.py
+++ webadmin/forms/guwen_duijie_biao.py
@@ -71,5 +71,69 @@ class OuterUpdateForm(forms.Form):
             "required": "类型错误！",
         }
     )
+# 内层判断添加
+class InnerCreateForm(forms.Form):
+    daozhang = forms.IntegerField(
+        required=True,
+        error_messages={
+            "required": "实际到账不能为空！",
+        }
+    )
+    start_time = forms.DateField(
+        required=True,
+        error_messages={
+            "required": "开始时间不能为空！",
+        }
+    )
+    stop_time = forms.DateField(
+        required=True,
+        error_messages={
+            "required": "结束时间不能为空！",
+        }
+    )
+    panduan_xinwenda = forms.BooleanField(
+        required=False,
+        error_messages={
+            "required": "是否为新问答不能为空！",
+        }
+    )
 
+    fugailiang = forms.IntegerField(
+        required=True,
+        error_messages={
+            "required": "覆盖量不能为空！",
+        }
+    )
 
+class InnerUpdateForm(forms.Form):
+    daozhang = forms.IntegerField(
+        required=False,
+        error_messages={
+            "required": "类型错误！",
+        }
+    )
+
+    fugailiang = forms.IntegerField(
+        required=False,
+        error_messages={
+            "required": "类型错误！",
+        }
+    )
+    start_time = forms.DateField(
+        required=False,
+        error_messages={
+            "required": "类型错误！",
+        }
+    )
+    stop_time = forms.DateField(
+        required=False,
+        error_messages={
+            "required": "类型错误！",
+        }
+    )
+    panduan_xinwenda = forms.BooleanField(
+        required=False,
+        error_messages={
+            "required": "类型错误！",
+        }
+    )
