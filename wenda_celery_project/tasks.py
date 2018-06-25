383ce20a31e06ea528ea90a17bf972296792de8a
zhaoxinpeng <15531506906@sina.cn>
2018-06-25 10:44:41 +0800

    问答 修复BUG celery task功能更改位置

--- wenda_celery_project/tasks.py
+++ wenda_celery_project/tasks.py
@@ -1622,62 +1622,6 @@ def weixin_daoqi_yonghu_tuisong():
     xiaoshou_weixin()
 
 
-# 更新机器人日志发布次数
-def robot_release_num():
-    now_date = datetime.datetime.today().strftime('%Y-%m-%d %H:00:00')
-    objs = models.RobotAccountLog.objects.filter(
-        lapse=False,
-        # create_date__gte = now_date
-    ).values(
-        'status',
-        'create_date',
-    ).annotate(Count('id'))
-    date_temp = {}
-    two_temp = {}
-    for obj in objs:
-        status = obj['status']
-        create_time = obj['create_date'].strftime('%Y-%m-%d %H')
-        id_count = obj['id__count']
-        if status in date_temp:
-            if create_time in date_temp[status]:
-                date_temp[status][create_time] += id_count
-            else:
-                date_temp[status][create_time] = id_count
-        else:
-            date_temp[status] = {
-                create_time: id_count
-            }
-        for key, val in date_temp.items():
-            for key1, val1 in val.items():
-                data_key = key1 + ':00:00'
-                if data_key in two_temp:
-                    if val1 in two_temp[data_key]:
-                        two_temp[data_key]['count'] += val1
-                    else:
-                        two_temp[data_key]['count'] = val1
-                else:
-                    two_temp[data_key] = {
-                        'count': val1
-                    }
-    for k, v in two_temp.items():
-        for k1, v1 in v.items():
-            create_date = k
-            count = v1
-            objs = models.RobotReleaseNum.objects.filter(
-                create_date=create_date,
-            )
-            if objs:
-                objs.update(
-                    create_date=create_date,
-                    robot_count=count
-                )
-            else:
-                models.RobotReleaseNum.objects.create(
-                    create_date=create_date,
-                    robot_count=count
-                )
-
-
 # 微信推送每日覆盖量
 @app.task
 def weixin_meiri_fugai_tuisong():
@@ -1782,6 +1726,62 @@ def weixin_meiri_fugai_tuisong():
     # guwen_weixin()
     print('销售 - - -- 》 ')
     xiaoshou_weixin()
+
+
+# 更新机器人日志发布次数
+def robot_release_num():
+    now_date = datetime.datetime.today().strftime('%Y-%m-%d %H:00:00')
+    objs = models.RobotAccountLog.objects.filter(
+        lapse=False,
+        # create_date__gte = now_date
+    ).values(
+        'status',
+        'create_date',
+    ).annotate(Count('id'))
+    date_temp = {}
+    two_temp = {}
+    for obj in objs:
+        status = obj['status']
+        create_time = obj['create_date'].strftime('%Y-%m-%d %H')
+        id_count = obj['id__count']
+        if status in date_temp:
+            if create_time in date_temp[status]:
+                date_temp[status][create_time] += id_count
+            else:
+                date_temp[status][create_time] = id_count
+        else:
+            date_temp[status] = {
+                create_time: id_count
+            }
+        for key, val in date_temp.items():
+            for key1, val1 in val.items():
+                data_key = key1 + ':00:00'
+                if data_key in two_temp:
+                    if val1 in two_temp[data_key]:
+                        two_temp[data_key]['count'] += val1
+                    else:
+                        two_temp[data_key]['count'] = val1
+                else:
+                    two_temp[data_key] = {
+                        'count': val1
+                    }
+    for k, v in two_temp.items():
+        for k1, v1 in v.items():
+            create_date = k
+            count = v1
+            objs = models.RobotReleaseNum.objects.filter(
+                create_date=create_date,
+            )
+            if objs:
+                objs.update(
+                    create_date=create_date,
+                    robot_count=count
+                )
+            else:
+                models.RobotReleaseNum.objects.create(
+                    create_date=create_date,
+                    robot_count=count
+                )
 
 
 # 下载关键词与关键词类型
