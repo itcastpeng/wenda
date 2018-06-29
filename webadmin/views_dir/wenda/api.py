dc55a38dc1446376197c9f65c14c270010f68c89
张聪 <18511123018@163.com>
2018-06-29 08:12:11 +0800

    调整缓存查覆盖关键词数据逻辑

--- webadmin/views_dir/wenda/api.py
+++ webadmin/views_dir/wenda/api.py
@@ -1662,16 +1662,24 @@ def keywords_cover_select_models(request):
         Q(Q(update_select_cover_date__isnull=True) | Q(update_select_cover_date__lt=now_date))
     )
 
-    keywords_objs = models.KeywordsTopSet.objects.select_related('client_user').filter(q).order_by(
+    # 先查询启用用户的关键词覆盖
+    keywords_objs = models.KeywordsTopSet.objects.select_related('client_user').filter(q).filter(
+        client_user__status=1
+    ).order_by(
         '?'
-    )[0:10].values('id', 'keyword', 'client_user_id', 'area')
+    )[0:2000].values('id', 'keyword', 'client_user_id', 'area')
+
+    if not keywords_objs:  # 如果启用的查询完了，则查询未启用的用户的关键词覆盖
+        keywords_objs = models.KeywordsTopSet.objects.select_related('client_user').filter(q).order_by(
+            '?'
+        )[0:2000].values('id', 'keyword', 'client_user_id', 'area')
 
     # 如果查询覆盖的词查完,则查询指定关键词中未查询的词
     if not keywords_objs:
         q = Q(Q(status=1) & Q(is_delete=False) & Q(
             Q(update_select_cover_date__isnull=True) | Q(update_select_cover_date__lt=now_date)))
         keywords_objs = models.KeywordsTopSet.objects.select_related('client_user').filter(q).order_by(
-            '?')[0:10].values('id', 'keyword', 'client_user_id')
+            '?')[0:2000].values('id', 'keyword', 'client_user_id')
 
     print('keywords_objs -->', keywords_objs)
     redis_data_list = {'data': []}
