0564be0a2445deeeabd2b9405d49fc3f784c977e
zhaoxinpeng <15531506906@sina.cn>
2018-06-30 11:01:58 +0800

    问答 覆盖报表覆盖不准确问题

--- wenda_celery_project/tasks.py
+++ wenda_celery_project/tasks.py
@@ -1209,18 +1209,17 @@ def update_client_covering_data():
 
         # 总覆盖数
         total_cover_num = 0
-        if client_user_id == 140:  # 西宁东方泌尿专科 删除词了,之前对应的覆盖也删除了,单独处理
-            userprofile_keywords_cover_objs = models.UserprofileKeywordsCover.objects.filter(
-                client_user_id=client_user_id)
-            for userprofile_keywords_cover_obj in userprofile_keywords_cover_objs:
-                total_cover_num += userprofile_keywords_cover_obj.cover_num
-
-        else:
-            # total_cover_num = obj['id__count']
-            total_cover_num = 0
-            objs = models.UserprofileKeywordsCover.objects.filter(client_user_id=obj['keywords__client_user_id'])
-            for obj in objs:
-                total_cover_num += obj.cover_num
+        # if client_user_id == 140:  # 西宁东方泌尿专科 删除词了,之前对应的覆盖也删除了,单独处理
+        userprofile_keywords_cover_objs = models.UserprofileKeywordsCover.objects.filter(
+            client_user_id=client_user_id)
+        for userprofile_keywords_cover_obj in userprofile_keywords_cover_objs:
+            total_cover_num += userprofile_keywords_cover_obj.cover_num
+        #
+        # else:
+        #     # total_cover_num = obj['id__count']
+        #     objs = models.UserprofileKeywordsCover.objects.filter(client_user_id=obj['keywords__client_user_id'])
+        #     for obj in objs:
+        #         total_cover_num += obj.cover_num
 
         keywords_topset_obj = models.KeywordsTopSet.objects.filter(
             client_user_id=obj["keywords__client_user_id"],
