980d03769d85954c82f27be7523231a02a238928
zhaoxinpeng <15531506906@sina.cn>
2018-07-03 20:55:17 +0800

    问答 生成指定关键词报表 顾问增加字段

--- wenda_celery_project/tasks.py
+++ wenda_celery_project/tasks.py
@@ -707,6 +707,11 @@ def keywords_top_page_cover_excel(user_id=None):
             if not keywords_top_info_objs:
                 continue
 
+            user_role = 5
+            if user_id:
+                user_objs = models.UserProfile.objects.filter(id=user_id)
+                user_role = user_objs[0].role_id
+
             wb = Workbook()
 
             ws = wb.active
@@ -720,7 +725,9 @@ def keywords_top_page_cover_excel(user_id=None):
             ws.cell(row=1, column=8, value="创建时间")
             ws.cell(row=1, column=9, value="更新时间")
             ws.cell(row=1, column=10, value="状态")
-
+            if user_role in [1,4,7]:
+                ws.cell(row=1, column=11, value="初始值")
+                ws.cell(row=1, column=12, value="当前值")
             url_publish_list = [i[0] for i in models.WendaRobotTask.objects.filter(task__release_user=user_obj,
                 wenda_type=2, ).values_list(
                 "wenda_url")]
@@ -764,6 +771,13 @@ def keywords_top_page_cover_excel(user_id=None):
 
                     keywords = "\n".join(keywords_list)
 
+                    initial_num = 0
+                    current_number = 0
+                    if user_role in [1,4,7]:
+                        if obj.initial_num:
+                            initial_num =   obj.initial_num
+                        if obj.current_number:
+                            current_number = obj.current_number
                     # print("-->", n, obj["id"], url, title, url__count, keywords, is_caina, huifu_num, flag_publish_str)
                     try:
                         ws.cell(row=n, column=1, value=n)
@@ -776,6 +790,9 @@ def keywords_top_page_cover_excel(user_id=None):
                         ws.cell(row=n, column=8, value=create_date)
                         ws.cell(row=n, column=9, value=update_date)
                         ws.cell(row=n, column=10, value=flag_publish_str)
+                        if user_role in [1,4,7]:
+                            ws.cell(row=n, column=11, value=initial_num)
+                            ws.cell(row=n, column=12, value=current_number)
 
                         n += 1
                     except IllegalCharacterError:
