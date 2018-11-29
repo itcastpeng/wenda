d721b53683443ddde97989da9572704977dfe340
张聪 <18511123018@163.com>
2018-11-29 15:17:46 +0800

    生成客户日覆盖报表 指定客户和日期

--- wenda_celery_project/tasks.py
+++ wenda_celery_project/tasks.py
@@ -1166,7 +1166,7 @@ def specified_userprofile_keywords_cover(user_id, generate_date, debug=False):
         'keywords__client_user__username',
         'keywords__client_user_id'
     ).annotate(Count("id"))
-
+    print('client_data -->', client_data)
     for user_obj in client_data:
         user_id = user_obj["keywords__client_user_id"]
         username = user_obj["keywords__client_user__username"]
